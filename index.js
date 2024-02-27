50616f8daeb16ad237b7019c115dd444d8dbf2e7
MUSTAFA MAZIN AHMED <mustafa.hamedi.2001@gmail.com>
2024-02-27 17:04:49 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob' ;
+
+function upperCaseCustomerName ()  {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    // Declaring a global variable from inside a function is generally not recommended
+    // this is to explore its consequences
+    bestCustomer = 'not bob';
+  }
+  
+  // Function to overwrite bestCustomer
+  function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+  }
+  
+  // Declare leastFavoriteCustomer constant in global scope
+  const leastFavoriteCustomer = 'kelly';
+  
+  // Function to try to change leastFavoriteCustomer (should throw an error)
+  function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'john';
+  }
